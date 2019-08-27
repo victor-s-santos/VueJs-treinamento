aabf58cb20f6b951295d558043868577e35b6517
victor-s-santos <victor.s.silva@unesp.br>
2019-08-27 13:08:18 -0300

    primeira insercao

--- projetos_vue_treinamento/vue-skills/src/App.vue
+++ projetos_vue_treinamento/vue-skills/src/App.vue
@@ -1,11 +1,27 @@
 <template>
- 
+  <div id="mydiv">
+    <Skills />
+  </div>
 </template>
 
 <script>
+import Skills from './components/Skills.vue'
 
+export default {
+  name: 'mydiv',
+  components: {
+    Skills
+  }
+}
 </script>
 
 <style>
-
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
