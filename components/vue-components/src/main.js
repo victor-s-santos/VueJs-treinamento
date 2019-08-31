23b1d12ad1d37ead53fefb0710426a87a593e655
victor-s-santos <victor.s.silva@unesp.br>
2019-08-31 18:43:33 -0300

    independencia dos components

--- components/vue-components/src/main.js
+++ components/vue-components/src/main.js
@@ -1,16 +1,28 @@
 import Vue from 'vue'
 
 Vue.component('vss-component', {
-  //necessita ser passada como função
   data: function(){
-    return {
-      nome: 'Victor Santos Silva'
-    }
-
+    return{
+      nome: 'Victor Santos Silva',
+      contador: 0,
+    } 
   },
-  template: '<h1>{{nome}}</h1>'
+  methods:{
+    incrementa: function(){
+      this.contador ++
+    }
+  },
+  template:`
+  <div>
+    <h1>Meu nome é: {{nome}}</h1>
+    <h2>{{contador}}</h2>
+    <button @click="incrementa">Clicado {{contador}} vezes</button>
+  </div>
+  `
 })
 
 new Vue({
   el: '#app',
 })
+//aqui se verifica que, apesar de haver 2 contador, eles são individuais, um
+//para cada component chamado
