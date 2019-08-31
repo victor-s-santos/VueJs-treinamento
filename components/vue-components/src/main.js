0c778e6aaee0211fcdff580cdc768d14181cf662
victor-s-santos <victor.s.silva@unesp.br>
2019-08-31 19:07:31 -0300

    component local e global

--- components/vue-components/src/main.js
+++ components/vue-components/src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 
+//assim se registra globalmente um component
+/*
 Vue.component('vss-component', {
   data: function(){
     return{
@@ -20,9 +22,39 @@ Vue.component('vss-component', {
   </div>
   `
 })
-
+*/
+//assim se registra localmente
+const vssComponent = {
+    data: function(){
+      return{
+        nome: 'Victor Santos Silva',
+        contador: 0,
+      } 
+    },
+    methods:{
+      incrementa: function(){
+        this.contador ++
+      }
+    },
+    template:`
+    <div>
+      <h1>Meu nome é: {{nome}}</h1>
+      <h2>{{contador}}</h2>
+      <button @click="incrementa">Clicado {{contador}} vezes</button>
+    </div>
+    `
+  }
 new Vue({
   el: '#app',
+  components: {
+    'vss-component': vssComponent
+  }
+})
+new Vue({
+  el:'#app2',
+  components:{
+    'vss-component': vssComponent
+  }
 })
 //aqui se verifica que, apesar de haver 2 contador, eles são individuais, um
 //para cada component chamado
