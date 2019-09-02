856950daf76e6a35c205a646e5817d2fea3d1877
victor-s-santos <victor.s.silva@unesp.br>
2019-09-02 15:35:59 -0300

    passando como computed propertie

--- comunicacao_entre_components/vue-components-communication/src/components/FilmesListaIten.vue
+++ comunicacao_entre_components/vue-components-communication/src/components/FilmesListaIten.vue
@@ -1,12 +1,18 @@
 <template>
     <li class="list-group-item">
-        <span>{{ filmeTitulo }}</span>
+        <!--<span>{{ filmeTitulo }}</span>-->
+        <span>{{filmeConcatenado}}</span>
         <button class="btn btn-success float-right">Editar</button>
     </li>
 </template>
 
 <script>
 export default {
-    props: ['filmeTitulo']
+    props: ['filmeTitulo'],
+    computed:{
+        filmeConcatenado(){
+            return `Título: ${this.filmeTitulo}`
+        }
+    }
 }
 </script>
