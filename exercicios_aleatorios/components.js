159870995e3ed1b6e09e0e5c6d81246bd0b6cace
victor-s-santos <victor.s.silva@unesp.br>
2019-09-04 12:56:04 -0300

    components globais e locais

--- exercicios_aleatorios/components.js
+++ exercicios_aleatorios/components.js
@@ -1,10 +1,46 @@
-Vue.component('menu-superior', {
-    data: function(){
-        return {
-            titulo: 'Botão Inicial'
+//components globais
+Vue.component('btn-contador', {
+    data:function(){
+        return{
+            contagem:0,
         };
     },
-    template: '<button>{{titulo}}</button>'
+    methods:{
+        incrementa: function(){
+            this.contagem++;
+        }
+    },
+    template: '<div><span>{{contagem}}x</span><button @click="incrementa">+1</button></div>'
+});
+
+Vue.component('contador-global', {
+    data:function(){
+        return{
+            x: 0,
+        }
+    },
+    methods:{
+        incrementa: function(){
+            this.x++;
+        }
+    },
+    template:'<div><button @click="incrementa"><h2>Clicado {{x}} vezes!</h2><button></div>'
+})
+
+//components locais
+
+let menusuperior = {//este component só pode ser chamado na div header
+    template:'<span>Cabeçalho</span>'
+};
+
+let header = new Vue({
+    el:'#header',
+    data:{
+        title:'Blablabla'
+    },
+    components:{
+        menusuperior: menusuperior
+    }
 });
 
 let app1 = new Vue({
