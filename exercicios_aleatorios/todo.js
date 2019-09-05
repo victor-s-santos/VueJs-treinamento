9d5ea15b53b93c3503c8aab5ba95350724e67f86
victor-s-santos <victor.s.silva@unesp.br>
2019-09-05 10:39:02 -0300

    estudando components

--- exercicios_aleatorios/todo.js
+++ exercicios_aleatorios/todo.js
@@ -1,44 +1,6 @@
-let tarefainput = {//component local
-    data:function(){
-        return{
-            listadetarefas: [],
-            tarefa: "",
-        }
-    },
-    methods:{
-        enviar: function(){
-            this.listadetarefas.push(this.tarefa);
-            this.tarefa = '';
-        }
-    },
-    template:`
-    <div>
-    <h2>
-        Insira as tarefas a serem computadas:
-    </h2>
-    <br>
-    <input v-model="tarefa" placeholder="Tarefa"/>
-    <button @click="enviar">Adiciona Tarefa</button>
-    </div>`
-
-};
-
-let cabecalho = new Vue({//primeira instância
-    el:'#mydiv1',
+let app1 = new Vue({
+    el:"#mydiv1",
     data:{
-        titulo: 'Usando Components',
-    }
-});
-
-let corpo = new Vue({//segunda instância
-    el: '#mydiv2',
-    data:{
-        informativo: 'Registrando sua lista de tarefas.',
-        listadetarefas: [],
-        tarefa: "",
-
+        nome: "Victor",
     },
-    components:{
-        tarefainput: tarefainput
-    }
 });
