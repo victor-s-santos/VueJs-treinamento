ced7af1b46a302017a7a0b23ada106a9fcf2908f
victor-s-santos <victor.s.silva@unesp.br>
2019-09-05 10:59:52 -0300

    component lista criado

--- exercicios_aleatorios/todo.js
+++ exercicios_aleatorios/todo.js
@@ -1,6 +1,23 @@
-let app1 = new Vue({
+let listadetarefas = {
+    props:['tarefas'],
+    template: `
+        <div>
+        <h1>Lista de tarefas</h1>
+        <ul>
+            <li v-for="tarefa in tarefas">{{tarefa}}</li>
+        </ul>
+        </div>
+    
+    `
+};
+
+let app1 = new Vue({//setada a primeira instância
     el:"#mydiv1",
     data:{
         nome: "Victor",
+        tarefas: ['varrer a casa', 'lavar louça'],
     },
+    components:{
+        listadetarefas
+    }
 });
