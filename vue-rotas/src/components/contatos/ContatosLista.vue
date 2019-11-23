682e02a3b897f2400f6a33c415e769fc71a31ded
victor-s-santos <victor.s.silva@unesp.br>
2019-11-23 17:15:36 -0300

    criado os components

---
+++ vue-rotas/src/components/contatos/ContatosLista.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h3 class="font-weight-light">Contatos mano</h3>
+        <button class="btn btn-secondary mb-4 mb-4" @click="voltar">Voltar</button>
+    </div>
+</template>
+
+<script>
+import ContatosListaUnitario from './ContatosListaUnitario.vue'
+
+export default {
+    components:{
+        ContatosListaUnitario
+    },
+    data(){
+        return {
+            contatos: [
+                {id: 1, nome: 'Marie Curie', email: 'marie@curie.com'},
+                {id: 2, nome: 'Nicola Tesla', email: 'nicola@tesla.com'},
+                {id: 3, nome: 'Leonardo Davinci', email: 'leonardo@davinci.com'},
+                {id: 4, nome: 'Isaac Newton', email: 'isaac@newton.com'},
+            ]
+        }
+    }
+}
+</script>
---
+++ vue-rotas/src/components/contatos/ContatosListaUnitario.vue
@@ -0,0 +1,17 @@
+<template>
+    <li>
+        <span>{{ contato.nome }}</span>
+        <button class="btn btn-info btn-sm float-right">Saber mais</button>
+    </li>
+</template>
+
+<script>
+export default {
+    props:{
+        contato: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
