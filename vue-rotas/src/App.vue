b78d941ab8980c4f1b71fccc96e0ee0c6a147897
victor-s-santos <victor.s.silva@unesp.br>
2019-11-23 15:19:07 -0300

    modificações iniciais para utilizar o bootstrap e remover hello.vue

--- vue-rotas/src/App.vue
+++ vue-rotas/src/App.vue
@@ -1,28 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <div class="jumbotron">
+      <h1 class="display-4">Vue Router</h1>
+      <p class="lead">Adicionando rotas a Single Page Applications no Vue.</p>
+    </div>
+
+    <div class="container">
+
+      <h3 class="font-weight-light">Contatos</h3>
+
+    </div>
+
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
