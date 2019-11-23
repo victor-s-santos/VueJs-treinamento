5b4c03f694220bc852666ac8f85b62a402973f18
victor-s-santos <victor.s.silva@unesp.br>
2019-11-23 16:55:27 -0300

    navegando programaticamente

--- vue-rotas/src/Views/contatos/Contatos.vue
+++ vue-rotas/src/Views/contatos/Contatos.vue
@@ -1,3 +1,16 @@
 <template>
-    <h3 class="font-weight-light">Contatos mano</h3>
+    <div>
+        <h3 class="font-weight-light">Contatos mano</h3>
+        <button class="btn btn-secondary mb-4 mb-4" @click="voltar">Voltar</button>
+    </div>
 </template>
+<script>
+export default {
+    methods:{
+        voltar(){
+            //this.$router.push({path: '/'}) outra possibilidade
+            this.$router.back()
+        }
+    }
+}
+</script>
