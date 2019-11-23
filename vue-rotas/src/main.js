0b992b7ad39ef1c762dd52402bb012e326894857
victor-s-santos <victor.s.silva@unesp.br>
2019-11-23 16:22:41 -0300

    extraindo as rotas de main.js para router.js

--- vue-rotas/src/main.js
+++ vue-rotas/src/main.js
@@ -1,20 +1,21 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+//import VueRouter from 'vue-router'
 import App from './App.vue'
-import Contatos from './Views/contatos/Contatos.vue'
-import Home from './Views/Home.vue'
+// import Contatos from './Views/contatos/Contatos.vue'
+// import Home from './Views/Home.vue'
+import router from './router'
 
 Vue.config.productionTip = false
 
-Vue.use(VueRouter)
+// Vue.use(VueRouter)
 
-const router = new VueRouter({
-  mode: 'history',
-  routes: [
-    { path: '/contatos', component: Contatos },
-    { path: '/', component: Home }
-  ]
-})
+// const router = new VueRouter({
+//   mode: 'history',
+//   routes: [
+//     { path: '/contatos', component: Contatos },
+//     { path: '/', component: Home }
+//   ]
+// })
 
 new Vue({
   router,
---
+++ vue-rotas/src/router.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Contatos from './Views/contatos/Contatos.vue'
+import Home from './Views/Home.vue'
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/contatos', component: Contatos },
+    { path: '/', component: Home }
+  ]
+})
