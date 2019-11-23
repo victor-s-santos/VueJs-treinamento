56ab95473a4954074e42c7a8b32d27bf673a1d0f
victor-s-santos <victor.s.silva@unesp.br>
2019-11-23 15:48:17 -0300

    rotas configuradas em main.js

--- vue-rotas/src/main.js
+++ vue-rotas/src/main.js
@@ -1,13 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
+import Contatos from './Views/contatos.vue'
+import Home from './Views/Home.vue'
 
 Vue.config.productionTip = false
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-  routes: []
+  routes: [
+    { path: '/contatos', component: Contatos },
+    { path: '/', component: Home }
+  ]
 })
 
 new Vue({
