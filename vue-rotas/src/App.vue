7f61b461be2b96bc5f87bcc2a90c38f01ad3d3df
victor-s-santos <victor.s.silva@unesp.br>
2019-11-23 16:42:09 -0300

    aplicando estilização nos botões em função das rotas

--- vue-rotas/src/App.vue
+++ vue-rotas/src/App.vue
@@ -7,8 +7,18 @@
     </div>
 
     <div class="container">
-      <router-link to="/" class="btn btn-info mb-2 mr-2">Home</router-link>
-      <router-link :to="{path: '/contatos'}" class="btn btn-info mb-2 mr-2">Contatos</router-link>
+      <router-link 
+        to="/" 
+        class="btn btn-info mb-2 mr-2"
+        exact-active-class="active">
+          Home
+        </router-link>
+      <router-link 
+        :to="{path: '/contatos'}" 
+        class="btn btn-info mb-2 mr-2"
+        active-class="active">
+          Contatos
+          </router-link>
 
       <router-view></router-view>
 
