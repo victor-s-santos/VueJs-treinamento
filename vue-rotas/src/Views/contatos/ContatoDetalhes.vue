fe04556469cf2b4a8b9a85fcfa2b5d5c461ffb2d
victor-s-santos <victor.s.silva@unesp.br>
2019-11-24 19:16:31 -0300

    usando interpolação de string com a rota

--- vue-rotas/src/Views/contatos/ContatoDetalhes.vue
+++ vue-rotas/src/Views/contatos/ContatoDetalhes.vue
@@ -1,3 +1,8 @@
 <template>
-    <h3 class="font-weight-light">Detatlhes do Contato</h3>
+    <h3 class="font-weight-light">Detatlhes do Contato {{$route.params.id}}</h3>
 </template>
+
+<script>
+export default {
+}
+</script>
