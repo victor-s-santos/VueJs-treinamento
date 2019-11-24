76971553b6aa6d2e61dd4c821e537b4a779083d7
victor-s-santos <victor.s.silva@unesp.br>
2019-11-24 19:47:31 -0300

    usando watchers eu consigo atribuir corretamente o valor da minha variável toda vez que novas rotas são acessadas

--- vue-rotas/src/Views/contatos/ContatoDetalhes.vue
+++ vue-rotas/src/Views/contatos/ContatoDetalhes.vue
@@ -8,6 +8,11 @@ export default {
         return {
             id: this.$route.params.id
         }
-    }
+    },
+    watch: {
+        '$route'(to, from){
+            this.id = to.params.id
+        }
+    },
 }
 </script>
