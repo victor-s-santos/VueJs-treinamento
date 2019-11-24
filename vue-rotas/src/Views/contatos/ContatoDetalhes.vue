8802dcdaebae4d491ab25eeb13c0e934935fa960
victor-s-santos <victor.s.silva@unesp.br>
2019-11-24 19:52:02 -0300

    usando beforerouterupdate como alternativa para resolver este problema, ao invés de usar watch

--- vue-rotas/src/Views/contatos/ContatoDetalhes.vue
+++ vue-rotas/src/Views/contatos/ContatoDetalhes.vue
@@ -9,10 +9,14 @@ export default {
             id: this.$route.params.id
         }
     },
-    watch: {
-        '$route'(to, from){
-            this.id = to.params.id
-        }
-    },
+    // watch: {
+    //     '$route'(to, from){
+    //         this.id = to.params.id
+    //     }
+    // },
+    beforeRouteUpdate(to, from, next){
+        this.id = to.params.id
+        next()
+    }
 }
 </script>
