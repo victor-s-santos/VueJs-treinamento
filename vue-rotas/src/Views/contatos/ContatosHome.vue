5ae7f74ed201a0c3cccd80d494877d9f455a834c
victor-s-santos <victor.s.silva@unesp.br>
2019-11-25 10:22:32 -0300

    component padrão criado na rota contatos

---
+++ vue-rotas/src/Views/contatos/ContatosHome.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h3 class="font-weight-light">Página home da lista de contatos.</h3>
+        <p>Visualização de detalhes e edição dos contatos.</p>
+    </div>
+</template>
--- vue-rotas/src/router.js
+++ vue-rotas/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Contatos from './Views/contatos/Contatos.vue'
+import ContatosHome from './Views/contatos/ContatosHome.vue'
 import Home from './Views/Home.vue'
 import ContatoDetalhes from './Views/contatos/ContatoDetalhes.vue'
 
@@ -11,7 +12,8 @@ export default new VueRouter({
   linkActiveClass: 'active',
   routes: [
     { path: '/contatos', component: Contatos, children: [
-      { path: ':id', component: ContatoDetalhes}
+      { path: ':id', component: ContatoDetalhes},
+      { path: '', component: ContatosHome}
     ] },
     { path: '/', component: Home }
   ]
