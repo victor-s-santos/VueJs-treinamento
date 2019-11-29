fb8aaa738dede49a6d3ec3a14afa7e9376afd132
victor-s-santos <victor.s.silva@unesp.br>
2019-11-29 10:33:48 -0300

    otimizando esse sistema

--- vue-rotas/src/router.js
+++ vue-rotas/src/router.js
@@ -20,11 +20,15 @@ const router = new VueRouter({
   mode: 'history',
   linkActiveClass: 'active',
   scrollBehavior(to, from, savedPosition){
+    if(savedPosition){
+      return savedPosition
+    }
     if(to.hash){
       return{
         selector: to.hash
       }
     }
+    return {x: 0, y: 0}
     
   },
   routes: [
