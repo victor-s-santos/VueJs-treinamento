f92e53473a718282d2a33f8a7a6f74cde7584368
victor-s-santos <victor.s.silva@unesp.br>
2019-12-01 18:01:21 -0300

    funcionalidade de editar contato implementada

--- vue-rotas/src/Views/contatos/ContatoEditar.vue
+++ vue-rotas/src/Views/contatos/ContatoEditar.vue
@@ -1,59 +1,71 @@
 <template>
     <div v-if="contato">
-        <h3 
-        class="font-weight-light">
-        Nome do Contato: {{contato.nome}}
-        </h3>
+        <h3 class="font-weight-light">Nome: {{ contato.nome }}</h3>
+
         <form @submit.prevent="salvar">
-            <div ckass="form-group">
-                <label>Nome:</label>
-                <input type="text"
-                    class="form-control"
-                    placeholder="Insira um nome"
+            <div class="form-group">
+                <label>Nome</label>
+                <input 
+                    type="text" 
+                    class="form-control" 
+                    placeholder="Insira o nome" 
                     v-model="contato.nome">
             </div>
             <div class="form-group">
-                <label>Email:</label>
-                <input type="email"
-                    class="form-control"
-                    placeholder="Insira um email"
+                <label>Email</label>
+                <input 
+                    type="email" 
+                    class="form-control" 
+                    placeholder="Insira o email"
                     v-model="contato.email">
             </div>
-            <button type="button"
-                class="btn btn-secondary mt-4 nb-4 mr-2"
-                @click="$router.back()">Voltar</button>
 
-        <button type="submit" 
-            class="btn btn-success mt-4 nb-4 mr-2">Salvar</button>
+            <button 
+                type="button" 
+                class="btn btn-secondary mt-4 mb-4 mr-2" 
+                @click="$router.back()">
+                    Voltar
+            </button>
+            <button 
+                type="submit" 
+                class="btn btn-success">
+                    Salvar
+            </button>
         </form>
+
     </div>
 </template>
+
 <script>
+
 import EventBus from './../../event-bus'
 
 export default {
     props: ['id'],
-    data(){
-        return{
-            contato: undefined
+    data() {
+        return {
+            contato: undefined,
+            estaCancelando: true
         }
     },
-    beforeRouteEnter(to, from, next){
-       next(vm => {
-           vm.contato = EventBus.buscarContato(+to.params.id)
-       })
-   },
-    beforeRouteLeave(to, from, next){
-        console.log('Executando um beforeRouteLeave! Será chamado sempre ao sair da página.')
-        const confirma = window.confirm('Você tem certeza que gostaria de sair da página?')
-        next(confirma)
+    beforeRouteEnter(to, from, next) {
+        next(vm => {
+            vm.contato = EventBus.buscarContato(+to.params.id)
+        })
     },
-    methods:{
-        salvar(){
-            EventBus.editarContato(this.contao)
+    beforeRouteLeave(to, from, next) {
+        console.log('beforeRouteLeave')
+        this.estaCancelando
+            ? next(window.confirm('Deseja realmente sair?'))
+            : next()
+    },
+    methods: {
+        salvar(event) {
+            EventBus.editarContato(this.contato)
+            this.estaCancelando = false
             this.$router.push('/contatos')
         }
-
     }
 }
 </script>
+
